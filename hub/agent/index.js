8cd5bdd1245bf5ef6705e7c3c108d5125b126bcb
Dojo Jon <dojo@outlook.ie>
2018-11-11 18:25:10 +0000

    agent mqtt

--- hub/agent/index.js
+++ hub/agent/index.js
@@ -1,1 +1,22 @@
 console.log("hello dojo");
+
+
+var mqtt = require('mqtt')
+
+var client  = mqtt.connect('mqtt://broker')
+ 
+client.on('connect', function () {
+  console.log('connected');
+  client.subscribe('presence', function (err) {
+    if (!err) {
+      client.publish('presence', 'Hello mqtt')
+    }
+  })
+})
+ 
+
+client.on('message', function (topic, message) {
+  // message is Buffer
+  console.log(message.toString())
+//  client.end()
+})
